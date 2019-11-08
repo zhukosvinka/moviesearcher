3f18485442fb3bab6d97706f117c5222708f6746
padoctb <yeahdima@gmail.com>
2019-11-08 17:26:23 +0200

    clean up

--- src/containers/PersonInfoContainer.js
+++ src/containers/PersonInfoContainer.js
@@ -12,8 +12,6 @@ const PersonInfoContainer = ({match, currentLang}) => {
   const isLoading = useSelector(({ personInfoReducer }) => personInfoReducer.isLoading);
   const personData = useSelector(({ personInfoReducer }) => personInfoReducer.data);
 
-  console.log('persobn')
-
   const personId = match.params.personId;
 
   useEffect(() => {
